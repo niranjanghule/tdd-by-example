02667644988531129a4d68c817d396cf04fc1130
niranjan <niranjan.ghule@gmail.com>
2021-09-01 23:52:20 +0530

    removed subclasses, refactored test cases

---
+++ src/main/java/org/sng/Money.java
@@ -0,0 +1,41 @@
+package org.sng;
+
+public class Money {
+    protected int amount;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public Money times(int multiplier){
+        return new Money(amount * multiplier,currency);
+    };
+
+    public static Money dollar(int amount) {
+        return new Money(amount,"USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Money(amount,"CHF");
+    }
+
+    public boolean equals(Object object){
+        Money money = (Money) object;
+        return amount == money.amount
+                && this.currency() == money.currency();
+    }
+
+    protected String currency;
+    public String currency() {
+        return currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+}
--- src/test/java/org/sng/MultiCurrencyMoneyTest.java
+++ src/test/java/org/sng/MultiCurrencyMoneyTest.java
@@ -12,23 +12,24 @@ import static org.junit.Assert.*;
 public class MultiCurrencyMoneyTest {
 
     @Test
-    public void testMuliplication() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15),five.times(3));
+    public void testMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15),five.times(3));
     }
 
     @Test
     public void testEquality(){
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         //Triangulation
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertFalse(Money.dollar(5).equals(Money.franc(5)));
     }
 
     @Test
-    public void testFrancMuliplication() {
-        Franc five = new Franc(5);
-        assertEquals(new Franc(10), five.times(2));
-        assertEquals(new Franc(15),five.times(3));
+    public void testCurrency(){
+        assertEquals("USD",Money.dollar(5).currency());
+        assertEquals("CHF",Money.franc(5).currency());
     }
+
 }
