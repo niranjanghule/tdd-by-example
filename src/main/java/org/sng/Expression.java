c633d1e73cddcae9a06c1fff553466692466c16c
niranjan <niranjan.ghule@gmail.com>
2021-09-05 11:56:58 +0530

    tests to add money with different currencies

--- src/main/java/org/sng/Expression.java
+++ src/main/java/org/sng/Expression.java
@@ -2,4 +2,8 @@ package org.sng;
 
 public interface Expression {
     Money reduce(Bank bank,String to);
+
+    Expression plus(Expression addend);
+
+    Expression times(int multiplier);
 }
--- src/main/java/org/sng/Money.java
+++ src/main/java/org/sng/Money.java
@@ -8,7 +8,7 @@ public class Money implements Expression {
         this.currency = currency;
     }
 
-    public Money times(int multiplier){
+    public Expression times(int multiplier){
         return new Money(amount * multiplier,currency);
     };
 
@@ -39,7 +39,7 @@ public class Money implements Expression {
                 '}';
     }
 
-    public Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
